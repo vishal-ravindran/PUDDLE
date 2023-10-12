1aea659263aec2d04a2eaeb179e528c40ff444f7
vishal-ravindran <104803633+vishal-ravindran@users.noreply.github.com>
2023-10-12 15:59:11 -0600

    created login page

--- puddle/core/urls.py
+++ puddle/core/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from core.forms import LoginForm
 from . import views
+from django.contrib.auth import views as auth_views # importing views from django authetication and renaming to avoid name clash with views
 
 app_name ='core'
 
@@ -7,4 +9,5 @@ urlpatterns = [
     path('', views.index, name='index'),
     path('contact/', views.contact, name='contact'),
     path('signup/', views.signup, name ='signup'),
+    path('login/', auth_views.LoginView.as_view(template_name = 'core/login.html', authentication_form = LoginForm), name ='login'),
 ]
