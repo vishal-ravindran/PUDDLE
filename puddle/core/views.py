c8da56307cf2154b09567edc6294ff9411545df8
vishal-ravindran <104803633+vishal-ravindran@users.noreply.github.com>
2023-10-12 12:45:10 -0600

    added signup form

--- puddle/core/views.py
+++ puddle/core/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from item.models import Category, Item
+from .forms import SignupForm
 
 
 # Create your views here.
@@ -14,3 +15,11 @@ def index(request):
 
 def contact(request):
     return render(request, 'core/contact.html')
+
+def signup(request):
+    form = SignupForm()
+    context = {
+        'form' : form,
+    }
+
+    return render(request, 'core/signup.html', context)
