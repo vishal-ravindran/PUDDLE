a03a6c90dcafe798f64a6aa734842398edc119d4
vishal-ravindran <104803633+vishal-ravindran@users.noreply.github.com>
2023-10-12 22:26:05 -0600

    created new app dashboard

--- puddle/item/views.py
+++ puddle/item/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import get_object_or_404, render
-
+from django.shortcuts import get_object_or_404, redirect, render
+from django.contrib.auth.decorators import login_required
 from item.models import Item
+from .forms import NewItemForm
 
 # Create your views here.
 
@@ -13,3 +14,25 @@ def detail(request, pk):
     }
 
     return render(request, 'item/detail.html', context)
+
+@login_required
+def new(request):
+    if request.method == 'POST':
+        form = NewItemForm(request.POST, request.FILES)
+
+        if form.is_valid():
+            item = form.save(commit=False)
+            item.created_by = request.user
+            item.save()
+
+            return redirect('item:detail', pk=item.id)
+        
+    else:
+        form = NewItemForm()
+
+        
+    context = {
+        'form': form,
+        'title': 'New Item',
+    }
+    return render(request, 'item/form.html', context)
