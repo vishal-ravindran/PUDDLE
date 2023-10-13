f8ec3435af361c5a71c34e33a78cb21672d6e36d
vishal-ravindran <104803633+vishal-ravindran@users.noreply.github.com>
2023-10-13 15:49:11 -0600

    added edit functionality to modify item details

--- puddle/item/forms.py
+++ puddle/item/forms.py
@@ -2,43 +2,82 @@ from django import forms
 from .models import Item
 
 INPUT_CLASSES = 'w-full px-6 py-4 rounded-xl border'
+
+
 class NewItemForm(forms.ModelForm):
 
     class Meta:
         model = Item
         fields = ('category', 'name', 'description', 'price', 'image')
 
-        widgets ={
+        widgets = {
 
-        'category' :forms.Select(attrs={
-        
-            'class': INPUT_CLASSES
-        
-        }),
-        
-        'name' :forms.TextInput(attrs={
-        
-            'class': INPUT_CLASSES
-        
-        }),
-        
-        'description' :forms.Textarea(attrs={
-        
-            'class': INPUT_CLASSES
-        
-        }),
-        
-        'price' :forms.TextInput(attrs={
-        
-            'class': INPUT_CLASSES
-        
-        }),
+            'category': forms.Select(attrs={
 
-        
-        'image' :forms.FileInput(attrs={
-        
-            'class': INPUT_CLASSES
-        
-        }),
+                'class': INPUT_CLASSES
+
+            }),
+
+            'name': forms.TextInput(attrs={
+
+                'class': INPUT_CLASSES
+
+            }),
+
+            'description': forms.Textarea(attrs={
+
+                'class': INPUT_CLASSES
+
+            }),
+
+            'price': forms.TextInput(attrs={
+
+                'class': INPUT_CLASSES
+
+            }),
+
+
+            'image': forms.FileInput(attrs={
+
+                'class': INPUT_CLASSES
+
+            }),
+
+        }
+
+
+
+class EditItemForm(forms.ModelForm):
+
+    class Meta:
+        model = Item
+        fields = ( 'name', 'description', 'price', 'image', 'is_sold')
+
+        widgets = {
+
+            'name': forms.TextInput(attrs={
+
+                'class': INPUT_CLASSES
+
+            }),
+
+            'description': forms.Textarea(attrs={
+
+                'class': INPUT_CLASSES
+
+            }),
+
+            'price': forms.TextInput(attrs={
+
+                'class': INPUT_CLASSES
+
+            }),
+
+
+            'image': forms.FileInput(attrs={
+
+                'class': INPUT_CLASSES
+
+            }),
 
         }
--- puddle/item/views.py
+++ puddle/item/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import get_object_or_404, redirect, render
 from django.contrib.auth.decorators import login_required
 from item.models import Item
-from .forms import NewItemForm
+from .forms import EditItemForm, NewItemForm
 
 # Create your views here.
 
@@ -36,6 +36,30 @@ def new(request):
         'title': 'New Item',
     }
     return render(request, 'item/form.html', context)
+
+@login_required
+def edit(request, pk):
+
+    item = get_object_or_404(Item, pk= pk, created_by = request.user)
+
+    if request.method == 'POST':
+        form = EditItemForm(request.POST, request.FILES, instance= item)
+
+        if form.is_valid():
+            item.save()
+
+            return redirect('item:detail', pk=item.id)
+        
+    else:
+        form = EditItemForm(instance=item)
+
+        
+    context = {
+        'form': form,
+        'title': 'Edit Item',
+    }
+    return render(request, 'item/form.html', context)
+
 
 @login_required
 def delete(request, pk):
