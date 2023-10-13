7f4cecc1016a67e64bbd5e2aee6e73a540e4a0a8
vishal-ravindran <104803633+vishal-ravindran@users.noreply.github.com>
2023-10-13 15:25:33 -0600

    added delete functionality

--- puddle/item/views.py
+++ puddle/item/views.py
@@ -36,3 +36,12 @@ def new(request):
         'title': 'New Item',
     }
     return render(request, 'item/form.html', context)
+
+@login_required
+def delete(request, pk):
+    item = get_object_or_404(Item, pk= pk, created_by = request.user)
+    item.delete()
+
+    return redirect('dashboard:index')
+
+
