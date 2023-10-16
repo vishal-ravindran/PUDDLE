7366a819a7f10f1ed37d8abfd439746e84e32b3b
vishal-ravindran <104803633+vishal-ravindran@users.noreply.github.com>
2023-10-16 13:50:53 -0600

    added browse view and search functionality to search items

--- puddle/item/views.py
+++ puddle/item/views.py
@@ -1,19 +1,37 @@
 from django.shortcuts import get_object_or_404, redirect, render
 from django.contrib.auth.decorators import login_required
-from item.models import Item
+from .models import Item
 from .forms import EditItemForm, NewItemForm
+from django.db.models import Q
 
 # Create your views here.
 
+
+def items(request):
+    items = Item.objects.filter(is_sold=False)
+    query = request.GET.get('query', '')
+
+    if query:
+        items = items.filter(Q(name__icontains=query)
+                             or Q(description__icontains=query))
+
+    return render(request, 'item/items.html', {
+        'items': items,
+        'query': query,
+    })
+
+
 def detail(request, pk):
     item = get_object_or_404(Item, pk=pk)
-    related_items = Item.objects.filter(category = item.category, is_sold = False).exclude(pk = pk)
+    related_items = Item.objects.filter(
+        category=item.category, is_sold=False).exclude(pk=pk)[0:3]
     context = {
         'item': item,
         'related_items': related_items,
     }
 
     return render(request, 'item/detail.html', context)
+
 
 @login_required
 def new(request):
@@ -26,34 +44,33 @@ def new(request):
             item.save()
 
             return redirect('item:detail', pk=item.id)
-        
+
     else:
         form = NewItemForm()
 
-        
     context = {
         'form': form,
         'title': 'New Item',
     }
     return render(request, 'item/form.html', context)
 
+
 @login_required
 def edit(request, pk):
 
-    item = get_object_or_404(Item, pk= pk, created_by = request.user)
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
 
     if request.method == 'POST':
-        form = EditItemForm(request.POST, request.FILES, instance= item)
+        form = EditItemForm(request.POST, request.FILES, instance=item)
 
         if form.is_valid():
             item.save()
 
             return redirect('item:detail', pk=item.id)
-        
+
     else:
         form = EditItemForm(instance=item)
 
-        
     context = {
         'form': form,
         'title': 'Edit Item',
@@ -63,9 +80,7 @@ def edit(request, pk):
 
 @login_required
 def delete(request, pk):
-    item = get_object_or_404(Item, pk= pk, created_by = request.user)
+    item = get_object_or_404(Item, pk=pk, created_by=request.user)
     item.delete()
 
     return redirect('dashboard:index')
-
-
